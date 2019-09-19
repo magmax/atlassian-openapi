91943e6132faf1a12fc21686513951639e66894d
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2019-09-19 08:23:17 +0200

    add python lib as module

--- generate.sh
+++ generate.sh
@@ -2,13 +2,16 @@
 
 lang=${1:-python}
 apis=${2:-$(find spec -name '*.yaml')}
-version=${3:-0.0.$(date +%y%m%d)}
+version=${3:-0.1.$(date +%y%m%d)}
 
-url=
+declare -A uris
+uris["bitbucket"]="https://pypi.org/project/bitbucket-openapi/"
+uris["jira"]="https://pypi.org/project/jira-openapi/"
 
 for api in $apis; do
   filename=$(basename ${api})
   apiname="${filename%.*}"
+  url=${uris[$apiname]}
   docker run --rm \
     -v ${PWD}:/local \
     openapitools/openapi-generator-cli generate \
@@ -16,5 +19,7 @@ for api in $apis; do
     -g ${lang}\
     --skip-validate-spec \
     --additional-properties=packageName=${apiname}openapi,projectName=${apiname}-openapi,packageVersion=${version},packageUrl=${url} \
-    -o /local/out/${lang}/${apiname}
+    -o /local/libs/${lang}/${apiname}
 done
+
+sudo chown -R $(whoami):$(whoami) libs 
